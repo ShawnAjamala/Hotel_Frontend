c94138a327fc44c58736a955b91a04e57a22111e
ShawnAjamala <shawnajamala1@gmail.com>
2026-06-23 00:03:32 +0300

    add routes for guest dashboard to app.jsx

--- grand_horizon_hotel/src/App.jsx
+++ grand_horizon_hotel/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicDashboard from './pages/PublicDashboard';
 import Auth from './pages/Auth';
+import GuestDashboard from './pages/GuestDashboard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<PublicDashboard />} />
         <Route path="/login" element={<Auth />} />
         <Route path="/register" element={<Auth />} />
+        <Route path="/guest/dashboard" element={<GuestDashboard />} />
       </Routes>
     </BrowserRouter>
   );
